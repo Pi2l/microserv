d4e962f2e159f0cc6cf48eea8522bbc08d226953
Mykhailo <nightmisha8@gmail.com>
2025-04-13 17:02:41 +0300

    lab2: added exception handler

---
+++ src/main/java/org/m/clinic/api/v1/shared/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package org.m.clinic.api.v1.shared;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler( ItemNotFoundException.class )
+  @ResponseStatus( HttpStatus.NOT_FOUND )
+  Map<String, String> handler(RuntimeException exception) {
+    return Map.of( "cause", exception.getMessage() );
+  }
+
+  @ExceptionHandler( IllegalArgumentException.class )
+  @ResponseStatus( HttpStatus.BAD_REQUEST )
+  Map<String, String> handler(IllegalArgumentException exception) {
+    return Map.of( "cause", exception.getMessage() );
+  }
+}
