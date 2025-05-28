0c3dfe09db431476730f4ad4668484d6f9122b4e
Mykhailo <nightmisha8@gmail.com>
2025-05-28 08:35:22 +0300

    lab6: added basic kafka

--- appointment-service/src/main/java/org/m/appointment/service/AppointmentService.java
+++ appointment-service/src/main/java/org/m/appointment/service/AppointmentService.java
@@ -1,7 +1,5 @@
 package org.m.appointment.service;
 
-import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
-import org.m.appointment.api.v1.appointment.AppointmentDto;
 import org.m.appointment.api.v1.shared.ItemNotFoundException;
 import org.m.appointment.api.v1.user.DoctorDto;
 import org.m.appointment.api.v1.user.PatientDto;
@@ -9,9 +7,13 @@ import org.m.appointment.model.Appointment;
 import org.m.appointment.repository.AppointmentRepository;
 import org.m.lib.repository.PrimaryRepository;
 import org.m.lib.service.CrudService;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -21,6 +23,8 @@ public class AppointmentService extends CrudService<Appointment, Long> {
 
   private final AppointmentRepository appointmentRepository;
   private final RestTemplate restTemplate;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  private final ObjectMapper objectMapper;
 
   @Override
   protected PrimaryRepository<Appointment, Long> getRepository() {
@@ -32,7 +36,9 @@ public class AppointmentService extends CrudService<Appointment, Long> {
     checkPresence(appointment.getPatientId(), "http://USER/api/v1/patients/", PatientDto.class);
     checkPresence(appointment.getDoctorId(), "http://USER/api/v1/doctors/", DoctorDto.class);
 
-    return super.create(appointment);
+    Appointment createdAppointment = super.create(appointment);
+    sendAppointmentToKafka(createdAppointment);
+    return createdAppointment;
   }
 
   @Override
@@ -40,7 +46,9 @@ public class AppointmentService extends CrudService<Appointment, Long> {
     checkPresence(appointment.getPatientId(), "http://USER/api/v1/patients/", PatientDto.class);
     checkPresence(appointment.getDoctorId(), "http://USER/api/v1/doctors/", DoctorDto.class);
 
-    return super.update(appointment);
+    Appointment updatedAppointment = super.update(appointment);
+    sendAppointmentToKafka(updatedAppointment);
+    return updatedAppointment;
   }
 
   private <T> void checkPresence(Long id, String url, Class<T> klass) {
@@ -57,6 +65,16 @@ public class AppointmentService extends CrudService<Appointment, Long> {
     if (entity == null) {
       System.err.println("Entity with id " + id + " not found. Class: " + klass.getSimpleName());
       throw new ItemNotFoundException(id);
+    }
+  }
+
+  private void sendAppointmentToKafka(Appointment appointment) {
+    try {
+      String appointmentJson = objectMapper.writeValueAsString(appointment);
+      kafkaTemplate.send("appointments", appointmentJson);
+      System.out.println("Sent appointment to Kafka: " + appointmentJson);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Error serializing appointment to JSON", e);
     }
   }
 }
