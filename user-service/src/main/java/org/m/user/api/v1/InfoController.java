43f8a0bbb915d94c6a40dbe2478aba6ac9faa3fd
Mykhailo <nightmisha8@gmail.com>
2025-06-06 08:24:16 +0300

    lab7: added actuator

--- user-service/src/main/java/org/m/user/api/v1/InfoController.java
+++ user-service/src/main/java/org/m/user/api/v1/InfoController.java
@@ -1,11 +1,13 @@
 package org.m.user.api.v1;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@RefreshScope
 @RestController
 @RequestMapping(InfoController.API_URL)
 @RequiredArgsConstructor
